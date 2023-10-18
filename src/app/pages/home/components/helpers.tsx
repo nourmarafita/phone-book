798b76caedb255a3c47cb86808c399202d97a009
Nourma Rafita Sari <nourma.vendor.pasti@pegadaian.co.id>
2023-10-18 20:36:33 +0700

    adding table

---
+++ src/app/pages/home/components/helpers.tsx
@@ -0,0 +1,216 @@
+import { objectKeys } from '@gilbarbara/helpers';
+import { PlainObject } from '@gilbarbara/types';
+
+import { variants as themeVariants } from '@gilbarbara/components/src/modules/theme';
+
+import { VariantWithTones } from '@gilbarbara/components/src/types';
+
+const base = ['normal', 'stretch'];
+const contentDistribution = ['space-around', 'space-between', 'space-evenly', 'stretch'];
+const contentPosition = ['center', 'end', 'flex-end', 'flex-start', 'start'];
+
+export const flexItems = ['baseline', ...base, ...contentPosition];
+export const flexContent = [...base, ...contentDistribution, ...contentPosition];
+export const tones = ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900'] as const;
+export const variants = [...objectKeys(themeVariants), 'black', 'white'] as const;
+export const variantsWithTones = [...objectKeys(themeVariants), 'black', 'white'].reduce<
+    Array<string>
+>(
+    (acc:any, variant:any) => {
+        if (variant === 'black' || variant === 'white') {
+            return [...acc, variant];
+        }
+
+        return [...acc, variant, ...tones.map(tone => `${variant}.${tone}`)];
+    },
+    ['']
+) as unknown as VariantWithTones[];
+
+const chromaticModeTitles = {
+  desktop_light: 'Desktop (light)',
+  desktop_dark: 'Desktop (dark)',
+  mobile_light: 'Mobile (light)',
+  mobile_dark: 'Mobile (dark)',
+};
+
+type ChromaticMode = keyof typeof chromaticModeTitles;
+
+export function addChromaticModes(...inputModes: [ChromaticMode, ...ChromaticMode[]]) {
+  const modes = inputModes.reduce<PlainObject<any>>((acc, mode) => {
+    const [viewport, appearance] = mode.split('_');
+
+    acc[chromaticModeTitles[mode]] = {
+      appearance,
+      viewport: viewport === 'mobile' ? 'mobile2' : 'responsive',
+    };
+
+    return acc;
+  }, {});
+
+  return {
+    chromatic: {
+      modes,
+    },
+  };
+}
+
+export function disableControl() {
+  return { control: false };
+}
+
+export function hideNoControlsWarning() {
+  return {
+    hideNoControlsWarning: true,
+  };
+}
+
+export function hideProps(...props: string[]) {
+  const fields: PlainObject = {
+    as: hideTable(),
+    theme: hideTable(),
+  };
+
+  props.forEach(d => {
+    fields[d] = hideTable();
+  });
+
+  return fields;
+}
+
+export function hideStoryFromDocsPage() {
+  return {
+    parameters: {
+      docs: {
+        disable: true,
+      },
+    },
+  };
+}
+
+export function hideTable() {
+  return {
+    table: { disable: true },
+  };
+}
+
+export function colorProps(
+  props: Array<'accent' | 'backgroundColor' | 'bg' | 'borderColor' | 'color'> = ['color'],
+) {
+  return props.reduce<PlainObject>((acc, prop) => {
+    acc[prop] = { control: 'select', options: variantsWithTones };
+
+    return acc;
+  }, {});
+}
+
+export function flexBoxProps() {
+  return {
+    align: { control: 'select', options: ['', ...flexItems] },
+    alignContent: { control: 'select', options: ['', ...flexContent] },
+    direction: {
+      control: 'select',
+      options: ['row', 'row-reverse', 'column', 'column-reverse'],
+    },
+    gap: {
+      control: 'text',
+    },
+    justify: { control: 'select', options: ['', ...flexContent] },
+    wrap: { control: 'select', options: ['nowrap', 'wrap', 'wrap-reverse'] },
+  };
+}
+
+export function flexItemProps() {
+  return {
+    alignSelf: { table: { category: 'Flex Item' } },
+    basis: { table: { category: 'Flex Item' } },
+    fill: { table: { category: 'Flex Item' } },
+    flex: { table: { category: 'Flex Item' } },
+    justifySelf: { table: { category: 'Flex Item' } },
+    order: { table: { category: 'Flex Item' } },
+  };
+}
+
+export function layoutProps(options?: { display: string }) {
+  const { display } = options ?? {};
+
+  return {
+    display: { control: 'text', defaultValue: display, table: { category: 'Layout' } },
+    height: { control: 'text', table: { category: 'Layout' } },
+    maxHeight: { control: 'text', table: { category: 'Layout' } },
+    maxWidth: { control: 'text', table: { category: 'Layout' } },
+    minHeight: { control: 'text', table: { category: 'Layout' } },
+    minWidth: { control: 'text', table: { category: 'Layout' } },
+    opacity: { control: 'number', table: { category: 'Layout' } },
+    overflow: { control: 'text', table: { category: 'Layout' } },
+    pointerEvents: { control: 'select', table: { category: 'Layout' } },
+    textAlign: { control: 'select', table: { category: 'Layout' } },
+    transform: { control: 'text', table: { category: 'Layout' } },
+    transformOrigin: { control: 'text', table: { category: 'Layout' } },
+    transition: { control: 'text', table: { category: 'Layout' } },
+    width: { control: 'text', table: { category: 'Layout' } },
+  };
+}
+
+export function marginProps() {
+  return {
+    margin: { control: 'select', table: { category: 'Spacing' } },
+    mb: { control: 'select', table: { category: 'Spacing' } },
+    ml: { control: 'select', table: { category: 'Spacing' } },
+    mr: { control: 'select', table: { category: 'Spacing' } },
+    mt: { control: 'select', table: { category: 'Spacing' } },
+    mx: { control: 'select', table: { category: 'Spacing' } },
+    my: { control: 'select', table: { category: 'Spacing' } },
+  };
+}
+
+export function paddingProps() {
+  return {
+    padding: { control: 'select', table: { category: 'Spacing' } },
+    pb: { control: 'select', table: { category: 'Spacing' } },
+    pl: { control: 'select', table: { category: 'Spacing' } },
+    pr: { control: 'select', table: { category: 'Spacing' } },
+    pt: { control: 'select', table: { category: 'Spacing' } },
+    px: { control: 'select', table: { category: 'Spacing' } },
+    py: { control: 'select', table: { category: 'Spacing' } },
+  };
+}
+
+export function positioningProps() {
+  return {
+    bottom: { control: 'text', table: { category: 'Positioning' } },
+    left: { control: 'text', table: { category: 'Positioning' } },
+    position: { control: 'select', table: { category: 'Positioning' } },
+    right: { control: 'text', table: { category: 'Positioning' } },
+    top: { control: 'text', table: { category: 'Positioning' } },
+    zIndex: { control: 'number', table: { category: 'Positioning' } },
+  };
+}
+
+export function radiusProps() {
+  return {
+    radius: {
+      control: 'select',
+      options: ['', false, 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'round'],
+    },
+  };
+}
+
+export function spacingProps() {
+  return {
+    ...marginProps(),
+    ...paddingProps(),
+  };
+}
+
+export function textOptionsProps() {
+  return {
+    bold: { control: 'boolean', table: { category: 'Text Options' } },
+    italic: { control: 'boolean', table: { category: 'Text Options' } },
+    letterSpacing: { control: 'text', table: { category: 'Text Options' } },
+    lineHeight: { control: 'text', table: { category: 'Text Options' } },
+    size: { control: 'select', table: { category: 'Text Options' } },
+    textDecoration: { control: 'text', table: { category: 'Text Options' } },
+    textTransform: { control: 'select', table: { category: 'Text Options' } },
+    wordSpacing: { control: 'text', table: { category: 'Text Options' } },
+  };
+}
--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,30 +1,307 @@
-import React from "react";
-import Table from "./components/Table";
-import { useQuery } from "@apollo/client";
-import { GET_CONTACT_LIST, Contact } from "../../gql/queries"; 
+import React, { ReactNode } from "react";
+// import Table from "./components/Table";
+// import { useQuery } from "@apollo/client";
+// import { GET_CONTACT_LIST, Contact } from "../../gql/queries";
+import { Simplify, StringOrNumber } from "@gilbarbara/types";
+import {
+  DataTable,
+  // defaultProps,
+} from "@gilbarbara/components";
+import { DataTableColumn } from "@gilbarbara/components";
+import {
+  ChangeEvent,
+  forwardRef,
+  useCallback,
+  // useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { useSetState } from "react-use";
+// import { removeAccents, request } from "@gilbarbara/helpers";
+import { removeAccents } from "@gilbarbara/helpers";
+// import { StringOrNull } from "@gilbarbara/types";
+// import { Meta, StoryObj } from "@storybook/react";
+import {
+  Anchor,
+  Box,
+  Button,
+  ButtonUnstyled,
+  ComponentWrapper,
+  Dialog,
+  Dropdown,
+  H1,
+  Icon,
+  Input,
+  NonIdealState,
+  Spacer,
+  Tag,
+  Text,
+} from "@gilbarbara/components";
+import { users } from "./components/fixtures";
+// import {
+//   addChromaticModes,
+//   colorProps,
+//   disableControl,
+//   flexItemProps,
+//   hideProps,
+//   layoutProps,
+//   radiusProps,
+//   spacingProps,
+// } from "./components/helpers";
+import {
+  DropdownOption,
+  // VariantWithTones,
+} from "@gilbarbara/components/src/types";
+
+export type DataTableRowContent =
+  | ReactNode
+  | { label: ReactNode; value: string };
+
+export type DataTableRow<T extends string> = Simplify<
+  Record<T, DataTableRowContent> & { id?: StringOrNumber }
+>;
+
+// type Story = StoryObj<typeof DataTable>;
+
+type WrapperColumns = "email" | "team" | "status" | "action";
+
+type WrapperState = typeof wrapperState;
+
+interface SharedState {
+  // eslint-disable-next-line react/no-unused-prop-types
+  search: string;
+  status: string;
+  team: string;
+}
+
+interface Props extends SharedState {
+  setState: (state: Partial<SharedState>) => void;
+}
+
+const wrapperState = {
+  loading: false,
+  search: "",
+  searchValue: "",
+  showDialog: false,
+  team: "",
+  status: "",
+};
+
+const teams = users.reduce((acc, user) => {
+  if (user.team && !acc.some((d) => user.team === d.label)) {
+    acc.push({
+      label: user.team,
+      value: user.team,
+    });
+  }
+
+  return acc;
+}, [] as DropdownOption[]);
+
+const statuses = [
+  { label: "Invites", value: "invites" },
+  { label: "Users", value: "users" },
+];
+
+const UserHeader = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { setState, status, team } = props;
+  const [searchValue, setSearchValue] = useState("");
+  const debounceTimeout = useRef<number>();
+
+  const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    clearTimeout(debounceTimeout.current);
+
+    setSearchValue(value);
+
+    debounceTimeout.current = window.setTimeout(() => {
+      setState({ search: value });
+    }, 750);
+  };
+
+  const handleChangeStatus = (options: DropdownOption[]) => {
+    const [selected] = options;
+    const nextStatus = selected ? (selected.value as string) : "";
+
+    setState({ status: nextStatus });
+  };
+
+  const handleChangeTeam = (options: DropdownOption[]) => {
+    const [selected] = options;
+    const nextTeam = selected ? (selected.value as string) : "";
+
+    setState({ team: nextTeam });
+  };
+
+  return (
+    <Spacer ref={ref} data-component-name="UserHeader" mb="lg">
+      <ComponentWrapper data-flex="1" prefix={<Icon name="search" size={24} />}>
+        <Input
+          // accent={accent}
+          name="name"
+          onChange={handleChangeInput}
+          placeholder="Search by username"
+          prefixSpacing
+          value={searchValue}
+        />
+      </ComponentWrapper>
+      <Dropdown
+        // accent={accent}
+        items={teams}
+        onChange={handleChangeTeam}
+        placeholder="Team"
+        showClearButton={!!team}
+        values={teams.filter((d) => d.value === team)}
+        width={180}
+      />
+      <Dropdown
+        items={statuses}
+        onChange={handleChangeStatus}
+        placeholder="Status"
+        showClearButton={!!status}
+        values={statuses.filter((d) => d.value === status)}
+        width={180}
+      />
+    </Spacer>
+  );
+});
 
 function Contacts() {
-    const { loading, error, data } = useQuery<{ contact: Contact[] }>(GET_CONTACT_LIST);
+  // const { accent } = props;
+  const [{ loading, search, showDialog, status, team }, setState] =
+    useSetState<WrapperState>(wrapperState);
+  const headerRef = useRef<HTMLDivElement>(null);
 
-    if (loading) {
-      return <p>Loading...</p>;
-    }
-  
-    if (error) {
-      return <p>Error: {error.message}</p>;
-    }
-  
-    if (!data || !data.contact) {
-      return <p>No data available.</p>;
-    }
+  const handleClickReset = useCallback(() => {
+    setState({ search: "", searchValue: "", status: "" });
+  }, [setState]);
 
-    const headers = ['first_name', 'created_at'];
-    const contactList = data.contact;
+  const handleClickDelete = useCallback(() => {
+    setState({ showDialog: true });
+  }, [setState]);
+
+  const handleClickCancel = () => {
+    setState({ showDialog: false });
+  };
+
+  const handleClickConfirmation = () => {
+    setState({ showDialog: false });
+  };
+
+  const columns = useMemo(() => {
+    const items: DataTableColumn<WrapperColumns>[] = [
+      { key: "email", title: "Nome / E-mail", size: 250 },
+      { key: "team", title: "Team", min: 150 },
+      { key: "status", title: "Status", min: 180 },
+      {
+        key: "action",
+        disableSort: true,
+        size: 48,
+        title: null,
+      },
+    ];
+
+    return items;
+  }, []);
+
+  const data = useMemo(() => {
+    return users
+      .filter((d) => {
+        const nameOrEmail = removeAccents(d.name || d.email).toLowerCase();
+
+        const searchFilter = search
+          ? nameOrEmail.includes(removeAccents(search).toLowerCase())
+          : true;
+        let statusFilter = true;
+        const teamFilter = team ? d.team === team : true;
+
+        if (status) {
+          statusFilter = status === "invites" ? !!d.code : !!d.id;
+        }
+
+        return searchFilter && statusFilter && teamFilter;
+      })
+      .map((user) => {
+        const row: DataTableRow<WrapperColumns> = {
+          id: user.id ?? user.code,
+          email: (
+            <>
+              <Text color={!user.name ? "gray" : undefined}>
+                {user.name || "Unnamed User"}
+              </Text>
+              <Anchor href={`mailto:${user.email}`} size="mid">
+                {user.email}
+              </Anchor>
+            </>
+          ),
+          team: <Text size="mid">{user.team || "--"}</Text>,
+          status: (
+            <Tag
+              bg={user.id ? "green" : "blue"}
+              iconAfter={user.id ? "check" : "hourglass"}
+              invert
+            >
+              {user.id ? "Active" : "Invite sent"}
+            </Tag>
+          ),
+          action: (
+            <ButtonUnstyled
+              data-code={user.code}
+              data-id={user.id}
+              onClick={handleClickDelete}
+            >
+              <Icon name="trash" />
+            </ButtonUnstyled>
+          ),
+        };
+
+        return row;
+      });
+  }, [handleClickDelete, search, status, team]);
+
+  const noResults = useMemo(() => {
+    return (
+      <NonIdealState icon="search" size="sm" type="no-results">
+        <Button onClick={handleClickReset} size="sm">
+          Reset filters
+        </Button>
+      </NonIdealState>
+    );
+  }, [handleClickReset]);
   return (
-    <div >
-      <header className="App-header" >
-        <Table headers={headers} data={contactList} />
-      </header>
+    <div>
+      <Box width="100%">
+        <H1>Members</H1>
+        <UserHeader
+          ref={headerRef}
+          // accent={accent}
+          search={search}
+          setState={setState}
+          status={status}
+          team={team}
+        />
+        <Box data-component-name="DataTableWrapper">
+          <DataTable<WrapperColumns>
+            // {...props}
+            columns={columns}
+            data={data}
+            defaultSortColumn="email"
+            disableScroll
+            loading={loading}
+            noResults={noResults}
+            scrollElement={headerRef.current}
+          />
+        </Box>
+        <Dialog
+          content="Do you want to remove this user?"
+          isActive={showDialog}
+          onClickCancel={handleClickCancel}
+          onClickConfirmation={handleClickConfirmation}
+          title="Remove user"
+        />
+      </Box>
     </div>
   );
 }
